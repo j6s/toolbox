23c7e89b63ee42beb546dd1ac93dd3751c2b8364
Johannes Hertenstein <johannes@nimius.net>
2014-04-28 09:28:24 +0200

    Autoloader now is more flexible

--- 02_autoloader/autoloader.php
+++ 02_autoloader/autoloader.php
@@ -1,11 +1,11 @@
 <?php
 /**
-* @author Johannes Hertenstein
-* @licence GNU / GPL
-* feel free to use this in your own project, thats what its for
-*
-* @see github.com/thephpjo/toolbox
-**/
+ * @author Johannes Hertenstein
+ * @licence GNU / GPL
+ * feel free to use this in your own project, thats what its for
+ *
+ * @see github.com/thephpjo/toolbox
+ **/
 namespace toolbox;
 /*
 * Autoloader
@@ -14,20 +14,26 @@ namespace toolbox;
 * classname must equal filename
 */
 class autoloader {
-	static function load($name){
-		if(!empty($name)){
-			// input as namespace with or without leading backslash
-			// add leading backslash, when not allready there (this is to bring all the inputs to the same format)
-			$name = str_replace("\\\\","\\","\\".$name);
+    static function load($name){
+        if(!empty($name)){
+            // input as namespace with or without leading backslash
+            // add leading backslash, when not allready there (this is to bring all the inputs to the same format)
+            $name = str_replace("\\\\","\\","\\".$name);
 
-			// delete starting slash
-			$name = substr($name,1);
-			// delete function name, if given
-			// $name = substr($name, 0, strpos($name,"::"));
-			$path = str_replace("\\","/",$name);
-			include($path.".php");
-		}
-	}	
+            // delete starting slash
+            $name = substr($name,1);
+
+            // i know globals are evil... but...
+            $autoloader_prefix = (!empty($GLOBALS["autoloader_prefix"])) ? $GLOBALS["autoloader_prefix"] : "../..";
+
+            $path = dirname(__FILE__)."/".$autoloader_prefix."/".str_replace("\\","/",$name).".php";
+
+            if(!file_exists($path)){
+                throw new \Exception("Autoloader: File ".$path." not found");
+            }
+            include_once($path);
+        }
+    }
 }
-spl_autoload_register(__NAMESPACE__."\autoloader::load");
+spl_autoload_register(__NAMESPACE__."\\autoloader::load");
 ?>
